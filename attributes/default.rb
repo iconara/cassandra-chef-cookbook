8f0c26e1d59b69fe0e84a43303a1ef473979a566
Theo <theo@iconara.net>
2013-01-07 22:34:04 +0100

    Moved some things around in the attributes file

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,26 +1,22 @@
 default[:cassandra] = {
-  :cluster_name => "Test Cluster",
-  :seeds => "127.0.0.1",
+  :cluster_name => 'Test Cluster',
   :tarball => {
-    :url => "http://www.eu.apache.org/dist/cassandra/1.2.0/apache-cassandra-1.2.0-bin.tar.gz",
-    :md5 => "bca870d48906172eb69ad60913934aee"
+    :url => 'http://www.eu.apache.org/dist/cassandra/1.2.0/apache-cassandra-1.2.0-bin.tar.gz',
+    :md5 => '94b7ac630034d8eb6bc9febbefb2bd3d'
   },
-  :user => "cassandra",
-  :jvm  => {
-    :xms => 32,
-    :xmx => 512
-  },
+  :user => 'cassandra',
+  :installation_dir => '/usr/local/cassandra',
+  :conf_dir         => '/etc/cassandra',
+  # commit log, data directory, saved caches and so on are all stored under the data root. MK.
+  :data_root_dir    => '/var/lib/cassandra',
+  :log_dir          => '/var/log/cassandra',
+  :run_dir          => '/var/run/cassandra',
+  # listen_address must be set to something real if you want nodes in a cluster to talk. TH
+  :listen_address   => 'localhost',
+  :rpc_address      => '0.0.0.0',
+  :seeds            => '127.0.0.1',
   :limits => {
     :memlock => 'unlimited',
     :nofile  => 48000
   },
-  :installation_dir => "/usr/local/cassandra",
-  :conf_dir         => "/etc/cassandra",
-  # commit log, data directory, saved caches and so on are all stored under the data root. MK.
-  :data_root_dir    => "/var/lib/cassandra",
-  :log_dir          => "/var/log/cassandra",
-  :run_dir          => "/var/run/cassandra",
-  # listen_address must be set to something real if you want nodes in a cluster to talk. TH
-  :listen_address   => "localhost",
-  :rpc_address      => "0.0.0.0"
 }
